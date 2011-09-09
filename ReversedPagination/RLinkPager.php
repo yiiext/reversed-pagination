89af304554238285c97c579d4cf4307fe1411581
Maxime <maximebaldin@gmail.com>
2011-09-09 21:32:58 +0400

    : bugfix with getPageRange() method

--- ReversedPagination/RLinkPager.php
+++ ReversedPagination/RLinkPager.php
@@ -35,8 +35,8 @@ class RLinkPager extends CLinkPager {
 		if(($page=$pageCount-$currentPage)>=$pageCount){
             $page=$pageCount-1;
         }
-		$buttons[]=$this->createPageButton($this->prevPageLabel,$page,self::CSS_PREVIOUS_PAGE,$currentPage<=0,false);
 
+		$buttons[]=$this->createPageButton($this->prevPageLabel,$page,self::CSS_PREVIOUS_PAGE,$currentPage<=0,false);
 		// internal pages
 		for($i=$endPage;$i>=$beginPage;--$i){
             $buttons[]=$this->createPageButton($i+1,$i,self::CSS_INTERNAL_PAGE,false,$i==$pageCount-1-$currentPage);
@@ -53,5 +53,21 @@ class RLinkPager extends CLinkPager {
 		$buttons[]=$this->createPageButton($this->lastPageLabel,0,self::CSS_LAST_PAGE,$currentPage>=$pageCount-1,false);
 
 		return $buttons;
+	}
+
+	/**
+	 * @return array the begin and end pages that need to be displayed.
+	 */
+	protected function getPageRange()
+	{
+		$currentPage=$this->getCurrentPage();
+		$pageCount=$this->getPageCount();
+		$beginPage=max(0, $pageCount-$currentPage-(int)($this->maxButtonCount/2));
+		if(($endPage=$beginPage+$this->maxButtonCount-1)>=$pageCount)
+		{
+			$endPage=$pageCount-1;
+			$beginPage=max(0,$endPage-$this->maxButtonCount+1);
+		}
+		return array($beginPage,$endPage);
 	}
 }
